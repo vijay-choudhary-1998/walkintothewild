3f3f4e80af78076109f43a42dc5f8630461430c2
fahadsws1234 <fahadsws1234@gmail.com>
2025-05-20 19:22:17 +0530

    loale password

--- resources/views/livewire/front/auth/login-component.blade.php
+++ resources/views/livewire/front/auth/login-component.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="new_login_page_section">
         <div class="new_login_page_logo">
-            <a href="{{route('home')}}">
+            <a href="{{ route('home') }}">
                 <img src="{{ asset('assets/images/loginlogo.png') }}" class="img-fluid logologin" alt="loginlogo">
             </a>
         </div>
@@ -12,28 +12,25 @@
                 <div class="col-lg-9 col-md-11 mb-3">
                     <label for="inputEmailAddress" class="form-label mb-0">Email Address</label>
                     <input type="email" class="form-control new_input new_input_margin" wire:model="email"
-                        id="inputEmailAddress" placeholder="Email Address" value=""
-                        autocomplete="email" autofocus>
+                        id="inputEmailAddress" placeholder="Email Address" value="" autocomplete="email"
+                        autofocus>
                     @error('email')
-                    <strong class="text-danger">{{ $message }}</strong>
+                        <strong class="text-danger">{{ $message }}</strong>
                     @enderror
                 </div>
                 <div class="col-lg-9 col-md-11 mb-3">
                     <label for="inputChoosePassword" class="form-label mb-0">Enter Password</label>
                     <div class="input-group" id="show_hide_password">
-                        <input
-                            type="password"
-                            class="form-control border-end-0 new_input new_input_margin"
-                            placeholder="Password"
-                            id="inputChoosePassword"
-                            wire:model="password"
+                        <input type="password" class="form-control border-end-0 new_input new_input_margin"
+                            placeholder="Password" id="inputChoosePassword" wire:model="password"
                             autocomplete="password">
-                        <a href="javascript:void(0);" class="input-group-text bg-transparent new_input_margin" onclick="togglePassword()">
-                            <i class='bx bx-hide' id="password-icon"></i>
+                        <a href="javascript:void(0);" class="input-group-text bg-transparent new_input_margin"
+                            onclick="togglePassword()">
+                            <i class='bi bi-eye-slash' id="password-icon"></i>
                         </a>
                     </div>
                     @error('password')
-                    <strong class="text-danger">{{ $message }}</strong>
+                        <strong class="text-danger">{{ $message }}</strong>
                     @enderror
                 </div>
                 <div class="col-md-6">
@@ -44,13 +41,14 @@
                     </div>
                 </div>
                 @if (session()->has('error'))
-                <div class="text-danger mt-2">{{ session('error') }}</div>
+                    <div class="text-danger mt-2">{{ session('error') }}</div>
                 @endif
                 <div class="col-lg-9 col-md-11 mb-md-0 mb-3">
                     <div class="d-grid">
                         <button type="submit" class="btn btn-primary  py-2" wire:loading.attr="disabled">
                             <span wire:loading.remove><i class="bx bxs-lock-open"></i>Log in</span>
-                            <span wire:loading><i class="spinner-border spinner-border-sm" role="status"></i> Loading...</span>
+                            <span wire:loading><i class="spinner-border spinner-border-sm" role="status"></i>
+                                Loading...</span>
                         </button>
                     </div>
                 </div>
@@ -58,21 +56,21 @@
         </div>
     </div>
 </div>
-@section('footerscript')
-<script>
-    function togglePassword() {
-        const passwordInput = document.getElementById("inputChoosePassword");
-        const passwordIcon = document.getElementById("password-icon");
+@script
+    <script>
+        function togglePassword() {
+            const passwordInput = document.getElementById("inputChoosePassword");
+            const passwordIcon = document.getElementById("password-icon");
 
-        if (passwordInput.type === "password") {
-            passwordInput.type = "text";
-            passwordIcon.classList.remove("bx-hide");
-            passwordIcon.classList.add("bx-show");
-        } else {
-            passwordInput.type = "password";
-            passwordIcon.classList.remove("bx-show");
-            passwordIcon.classList.add("bx-hide");
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                passwordIcon.classList.remove("bi-eye-slash");
+                passwordIcon.classList.add("bi bi-eye");
+            } else {
+                passwordInput.type = "password";
+                passwordIcon.classList.remove("bi bi-eye");
+                passwordIcon.classList.add("bi-eye-slash");
+            }
         }
-    }
-</script>
-@endsection
+    </script>
+@endscript
